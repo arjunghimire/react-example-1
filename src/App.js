c9527fa32e177351f4a99e4ea93a954109d847d2
Arjun Ghimire <arjunghimire0714@gmail.com>
2018-04-18 23:28:47 +0545

    static route basic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
+import Routerexample from "./components/Routerexample";
+
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = { term: ''};
-  }
-
   render() {
     return (
       <div>
-       <h4> value of input : { this.state.term} </h4> 
-       <input onChange={ event => this.setState({term: event.target.value})} />
+       <Routerexample />
       </div>     
     );
   }
